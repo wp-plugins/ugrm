ae7e1b39cda3e550347073b2f165d2c0b7046886
warren.brown <warren.brown@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-01-17 19:51:02 +0000

    Version 1.6 important bug fix for redirect login target to SSL and IIS support
    git-svn-id: https://plugins.svn.wordpress.org/ugrm/tags/1.6@491355 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- UGRM.php
+++ UGRM.php
@@ -3,7 +3,7 @@
 Plugin Name: UGRM - UFAD Groups to Roles Munger
 Plugin URI: http://www.flmnh.ufl.edu/omt/omtforge
 Description: Andy & Warren's University of Florida Shibboleth UFAD Groups Munger for Wordpress
-Version: 1.5
+Version: 1.6
 Author: Warren Hypnotoad Brown
 Author URI: http://www.flmnh.ufl.edu/omt/omtforge
 License: GPL2
@@ -12,13 +12,22 @@ License: GPL2
 require_once dirname(__FILE__) . '/options.php';
 
 function UGRM_munge_UFAD_Groups2Roles($user_role) {
-    if (isset($_SERVER['UFADGroupsDN'])) {
+    if (isset($_SERVER['UFADGroupsDN']) || isset($_SERVER['HTTP_UFADGROUPSDN'])) {
         $UGRM_admin_role = get_option('UGRM_admin_role');
         $UGRM_editor_role = get_option('UGRM_editor_role');
         $UGRM_author_role = get_option('UGRM_author_role');
         $UGRM_contributor_role = get_option('UGRM_contributor_role');
         $UGRM_subscriber_role = get_option('UGRM_subscriber_role');
-        $UFADGroupsDN = $_SERVER['UFADGroupsDN'];
+        //IIS prepends a HTTP_ prefix to all the Shibboleth server variables
+        //because Shibboleth sends them through CGI as IIS does not
+        //support envirnoment variables. See https://wiki.shibboleth.net/confluence/display/SHIB2/NativeSPAttributeAccess
+        //for details. Thus we check for IIS.
+        if (strpos($_SERVER['SERVER_SOFTWARE'], 'IIS')) {
+            $UFADGroupsDN = $_SERVER['HTTP_UFADGROUPSDN']; 
+        }        
+        else {
+            $UFADGroupsDN = $_SERVER['UFADGroupsDN'];    
+        }
         if (strpos($UFADGroupsDN, $UGRM_admin_role)) {
             $user_role = "administrator";
         }
@@ -45,7 +54,9 @@ add_filter ( 'shibboleth_user_role', 'UGRM_munge_UFAD_Groups2Roles' );
 
 function UGRM_munge_return_target_to_HTTPS($initiator_url) {
     if(get_option('UGRM_return_target_to_HTTPS')=="checked") {
-        $initiator_url=str_replace('target=http','target=https',$initiator_url);
+       if (!strpos($initiator_url,'target=https') ){
+            $initiator_url=str_replace('target=http','target=https',$initiator_url);
+       }
         return $initiator_url;
     }
     else {
